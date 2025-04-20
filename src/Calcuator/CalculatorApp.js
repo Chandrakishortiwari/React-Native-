7619d0bec80094fafd9e08cbaf3a2c618c0ead2c
chandrakishor <chandrakishoret197@gmail.com>
2025-04-20 13:35:53 +0530

    work on dark mode and light mode

--- src/Calcuator/CalculatorApp.js
+++ src/Calcuator/CalculatorApp.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Switch, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 const CalculatorApp = () => {
-  return (
-    <View>
-      <Text>CalculatorApp</Text>
-    </View>
-  )
-}
+  const [dark, setDark] = useState(false);
 
-export default CalculatorApp
+  return (
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        paddingVertical: 20,
+      
+        backgroundColor: dark ? '#000' : '#fff',
+      }}
+    >
+      <Text style={{ color: dark ? '#fff' : '#000', fontSize: 24, marginBottom: 20 }}>
+        {dark ? 'Dark Mode' : 'Light Mode'}
+      </Text>
+
+      <Switch
+        value={dark}
+        onValueChange={() => setDark(!dark)}
+        thumbColor={dark ? '#fff' : '#000'}
+        trackColor={{ true: '#81b0ff', false: '#767577' }}
+      />
+    </View>
+  );
+};
+
+export default CalculatorApp;
